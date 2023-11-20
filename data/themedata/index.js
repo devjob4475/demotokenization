bf5e5429a3f28bf9e5d7aa8cc9d0f1dd2c45619e
devjob4475 <147500581+devjob4475@users.noreply.github.com>
2023-11-20 10:19:51 +0700

    v.1

---
+++ data/themedata/index.js
@@ -0,0 +1,19 @@
+export const themedata = [
+    {
+        "primary": "#84BAA1",
+        "secondary": "#48846B",
+        "three"  : "#ffffff",
+        "four":"#7F8391",
+        "five"  : "#52752F",
+        "six"  : "#307BF1",
+        "seven"  : "#EBEBEB",
+        "eight"  : "'#666666'",
+        "nine"  : "'#97BDFE'",
+        "ten"  : "#171717",
+        "eleven"  : "#9CD6B3",
+        "twelve"  : "#D9D9D9",
+        "bgshadowwhite":"#FFFBE2",
+        "greenlight":"#90BA61",
+        "greenblack":"#44612A"
+    }
+];
